193a2c00b820559a847dac4fe6cf42e0debbce1d
Yuchen Pei <me@ypei.me>
2019-01-02 14:23:29 +0100

    minor edit

--- shapley-values/shapley.py
+++ shapley-values/shapley.py
@@ -14,7 +14,6 @@ def shapley(n, v):
     for i in range(1, n - 1):
         coef[i] = i * coef[i - 1] / (n - i)
     coef[n - 1] = 1 / n
-    coef[n] = 0
     for i in range(n):
         to_or = 1 << i
         for j in range(2 ** n):
